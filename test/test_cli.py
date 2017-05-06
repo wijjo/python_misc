04bc139f184807a285044d6c07a572bf08a27cda
Steve Cooper <github@wijjo.com>
2017-05-06 16:29:58 -0700

    copyright date

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2016 Steven Cooper
+# Copyright 2016-17 Steven Cooper
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
